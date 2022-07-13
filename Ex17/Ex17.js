0ffb2c52ea564819c4c5030dc7cb83e197f4c108
unknown <mohamedhelalilyes@gmail.com>
2022-07-13 19:57:01 +0200

    Exercise 17 : Make a todo list mini app using just javascript and console

---
+++ Ex17/Ex17.js
@@ -0,0 +1,34 @@
+const todoList = [];
+
+while(true)
+{
+    let whatToDo = prompt("What will you do to your list?").toLowerCase().trim();
+    if(whatToDo === "new")
+    {
+        let newToDo = prompt("Add new todo to your list");
+        todoList.push(newToDo);
+        console.log(newToDo, " added to list");
+    }
+    else if(whatToDo === "list")
+    {
+        console.log("*********");
+        for(let x in todoList)
+        {
+            console.log(`${x}: ${todoList[x]}`);
+        }
+        console.log("*********");
+    }
+    else if(whatToDo === "delete")
+    {
+        let remToDo = prompt("Remove a todo from your list by typing the index");
+        if(todoList.splice(remToDo, 1).length > 0)
+            console.log("Todo removed");
+        else
+            console.log("Todo doesn't exist");
+    }
+    else if(whatToDo === "quit")
+    {
+        console.log("Ok, you quit the app");
+        break;
+    } 
+}
