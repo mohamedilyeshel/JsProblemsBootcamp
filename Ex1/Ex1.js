cfa3371fa37b55cd3dc110efb2bd40e9c6272044
unknown <mohamedhelalilyes@gmail.com>
2022-07-24 14:53:38 +0200

    Exercise 18: Make a pokemon generator

--- Ex1/Ex1.js
+++ Ex1/Ex1.js
@@ -1,3 +1,46 @@
 let input = [1,2,3];
 let output = input.reverse();
 console.log(output);
+
+// const nums = 
+// {
+//     0: 3,
+//     1: 2000,
+//     2: 1200,
+//     3: 2,
+//     4: 300,
+//     5: 100,
+//     6: 12000,
+//     7: 7,
+//     8: 50,
+//     length: 9,
+//     foreac : function (fun) {
+//         for(let i = 0; i < this.length; i++)
+//         {
+//             fun(this[i])
+//         }
+//     },
+//     sorted : function (fun)
+//     {
+//         for(let i = 0; i < this.length; i++)
+//         {
+//             for(let j = i+1; j < this.length; j++)
+//             {
+//                 if(fun(this[i], this[j]) > 0)
+//                 {
+//                     let aux = this[i];
+//                     this[i] = this[j];
+//                     this[j] = aux;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// nums.foreac((x) => {
+//     console.log(x);
+// })
+
+// nums.sorted((a,b) => {
+//     return b-a;
+// })
---
+++ Ex18/Ex18.js
@@ -0,0 +1,36 @@
+const createElem = (elemType, sibOrParentElem, position, attributeType = "", attributeVal) => 
+{
+    const elem = document.createElement(elemType);
+    if(attributeType !== "")
+    {
+        elem.setAttribute(attributeType, attributeVal);
+    }
+    sibOrParentElem.insertAdjacentElement(position, elem);
+    return elem;
+}
+
+let pokNum = parseInt(prompt("How many pokemon you want to see?"));
+
+while((isNaN(pokNum)) || (pokNum < 0))
+{
+    pokNum = parseInt(prompt("How many pokemon you want to see?"));
+}
+
+const pageTitle = document.querySelector("h1");
+const urlPok = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+
+if(pokNum === 0)
+{
+    const msg = createElem("p",pageTitle, "afterend")
+    msg.innerText = "No Pokemon Found";
+}
+else
+{
+    let parentDiv = createElem("div", pageTitle, "afterend", "id", "pokemonList");
+    for(let i = pokNum; i > 0; i--)
+    {
+        let item = createElem("div", parentDiv, "afterbegin", "class", "item");
+        let img = createElem("img", item, "afterbegin", "src", `${urlPok}${i}.png`);
+        let num = createElem("span", img, "afterend").innerText = i;
+    }
+}
